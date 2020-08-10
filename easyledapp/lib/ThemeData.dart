220e524461fb7036266037d01e341fc7d54415f4
Victor Boesen <victorboesen@hotmail.com>
2020-08-10 14:12:35 +0200

    :heavy_check_mark: Fixed ugly theme

--- easyledapp/lib/ThemeData.dart
+++ easyledapp/lib/ThemeData.dart
@@ -1,62 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// Boolean that stores if dark mode is on or off
-bool isLightTheme = true;
-// Function that updates the light/dark mode boolean with new value
-// Reassigns new bool value in "Shared preferences" to load either light/dark when opening app
-updateTheme() async {
-  isLightTheme = !isLightTheme;
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setBool('isLightTheme', isLightTheme);
-}
-
 //---------------------------------COLORS-------------------------------------//
 
 // TODO: Fix ugly color theme
-Color themeBlue = Color.fromRGBO(0, 170, 255, 1);
-Color themeOrange = Color.fromRGBO(255, 85, 0, 1);
+Color defaultBlue = Color.fromRGBO(0, 12, 46, 1);
+Color defaultOrange = Color.fromRGBO(238, 178, 116, 1);
 
 Color redLight = Color.fromRGBO(255, 0, 0, 1);
 Color greenLight = Color.fromRGBO(0, 255, 0, 1);
 Color blueLight = Color.fromRGBO(0, 0, 255, 1);
 
-//---------------------------------THEMES-------------------------------------//
-
-// Light theme uses default black as text color except for Appbar title
-ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  appBarTheme: AppBarTheme(
-      color: themeOrange,
-      iconTheme: IconThemeData(
-          color: themeBlue
-      ),
-    textTheme: TextTheme(
-      headline1: TextStyle(fontSize: 30),
-    )
-  ),
-  textTheme: TextTheme(
-    headline1: TextStyle(fontSize: 25),
-    subtitle1: TextStyle(fontSize: 20),
-    subtitle2: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-    bodyText1: TextStyle(fontSize: 15)
-  ),
-);
+//---------------------------------THEME--------------------------------------//
 
 
-// Dark theme uses primary color as text
-ThemeData darkTheme = ThemeData(
+ThemeData defaultTheme = ThemeData(
+  primaryColor: defaultBlue,
+  accentColor: defaultOrange,
   brightness: Brightness.dark,
   appBarTheme: AppBarTheme(
-      color: themeBlue,
+      color: defaultBlue,
       iconTheme: IconThemeData(
-          color: themeOrange
+          color: defaultOrange
       ),
   ),
   textTheme: TextTheme(
-      headline1: TextStyle(fontSize: 25, color: themeBlue),
-      subtitle1: TextStyle(fontSize: 20, color: themeBlue),
-      subtitle2: TextStyle(fontSize: 15, color: themeBlue, fontWeight: FontWeight.bold),
-      bodyText1: TextStyle(fontSize: 15, color: themeBlue)
+      headline1: TextStyle(fontSize: 30, color: defaultOrange, fontWeight: FontWeight.bold),
+      subtitle1: TextStyle(fontSize: 20, color: Colors.white),
+      subtitle2: TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold),
+      bodyText1: TextStyle(fontSize: 15, color: Colors.white)
   ),
 );
